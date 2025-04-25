00f4676899d593033c23f47c93cbe30a31b7ef3e
Haryu2311 <kakasa901@gmail.com>
2025-04-25 12:01:47 +0700

    add

--- BE/src/index.js
+++ BE/src/index.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 const Router = require('./router/main');
 db.connect();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://comicstore.lovestoblog.com/'  // hoặc '*' cho phép tất cả
+  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 Router(app);
